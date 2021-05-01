0f67e25966880938d545ba75b0e145e048e55a6b
Rob <rob.readling@gmail.com>
2021-05-01 16:43:52 -0400

    finished problem 42

--- ex42.py
+++ ex42.py
@@ -26,13 +26,51 @@ class Person(object):
         ## Person has- pet of some kind
         self.pet = None
 
-    ## ??
-    class Employee(Person):
+## ??
+class Employee(Person):
 
-        def __init__(self, name, salary):
-            ## ?? hmm what is this strange magic?
-            super(Employee, self).__init__(name)
-            ## ??
-            self.salary = salary
+    def __init__(self, name, salary):
+        ## ?? hmm what is this strange magic?
+        super(Employee, self).__init__(name)
+        ## ??
+        self.salary = salary
 
 ## ??
+class Fish(object):
+     pass
+
+## ??
+class Salmon(Fish):
+    pass
+
+## ??
+class Halibut(Fish):
+    pass
+
+
+## rover is-a Dog
+rover = Dog("Rover")
+
+## ??
+satan = Cat("Satan")
+
+## ??
+mary = Person("Mary")
+
+## ??
+mary.pet = satan
+
+## ??
+frank = Employee("Frank", 120000)
+
+## ??
+frank.pet = rover
+
+## ??
+flipper = Fish()
+
+## ??
+crouse = Salmon()
+
+## ??
+harry = Halibut()
